554e17db5e18e7e9c6a79beb7639552535055890
qdlmcfresh <qdlmcfresh@gmail.com>
2023-07-12 13:31:43 +0200

    Add nix flake files

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  description = "bw-key";
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        myRustBuild = rustPlatform.buildRustPackage {
+          pname =
+            "bw-key"; # make this what ever your cargo.toml package.name is
+          version = "0.1.1";
+          src = ./.; # the folder with the cargo.toml
+          nativeBuildInputs = [ pkgs.pkg-config ]; # just for the host building the package
+          buildInputs = [ pkgs.openssl ]; # packages needed by the consumer
+          cargoLock.lockFile = ./Cargo.lock;
+          doCheck = false;
+        };
+      in
+      {
+        defaultPackage = myRustBuild;
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" ]; }) pkgs.pkg-config pkgs.openssl ];
+        };
+      });
+
+}
